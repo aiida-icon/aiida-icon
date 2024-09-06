59e213dc01f523054e284779ecb7ad795372f5f7
DropD <rico.haeuselmann@gmail.com>
2024-09-06 17:14:23 +0200

    enable to submit R02B04 case

--- src/aiida_icon/calculations.py
+++ src/aiida_icon/calculations.py
@@ -4,6 +4,7 @@ import pathlib
 import re
 import typing
 
+import f90nml
 from aiida import engine, orm
 from aiida.common import datastructures, folders
 from aiida.parsers import parser
@@ -18,62 +19,20 @@ class Icon(engine.CalcJob):
     @classmethod
     def define(cls, spec: calcjob.CalcJobProcessSpec) -> None:  # type: ignore[override] # forced by aiida-core
         super().define(spec)
-        spec.input("expid", valid_type=orm.Str, serializer=orm.to_aiida_type)
-        spec.input("ifs2icon_filename", valid_type=orm.Str, serializer=orm.to_aiida_type)
-        spec.input("gcm_converted", valid_type=orm.RemoteData, help="Converted GCM data.")
-        spec.input(
-            "boundary_data",
-            valid_type=orm.RemoteData,
-            help="Base directory for boundary data.",
-        )
-        spec.input(
-            "inidata",
-            valid_type=orm.RemoteData,
-            help="Initial files.",
-        )
-        spec.input("ini_basedir", valid_type=orm.RemoteData)
-        spec.input(
-            "restart_file_dir",
-            required=False,
-            valid_type=orm.RemoteData,
-            help="Remote dir containing the restart file.",
-        )
-        spec.input(
-            "restart_file_name",
-            required=False,
-            valid_type=orm.Str,
-            serializer=orm.to_aiida_type,
-            help="Name of the restart file (or path relative to `restart_file_dir`).",
-        )
         spec.input("master_namelist", valid_type=orm.SinglefileData)
         spec.input("model_namelist", valid_type=orm.SinglefileData)
+        spec.input("wrapper_script", valid_type=orm.SinglefileData, required=False)
         spec.input(
-            "lam_grid_relpath",
-            valid_type=orm.Str,
-            serializer=orm.to_aiida_type,
-            help="Relative to ini_basedir",
+            "dynamics_grid_file",
+            valid_type=orm.RemoteData,
         )
-        spec.input(
-            "parent_grid_relpath",
-            valid_type=orm.Str,
-            serializer=orm.to_aiida_type,
-            help="Relative to ini_basedir",
-        )
-        spec.input(
-            "extpar_relpath",
-            valid_type=orm.Str,
-            serializer=orm.to_aiida_type,
-            help="Relative to ini_basedir",
-        )
-        spec.input(
-            "ghg_file_relpath",
-            valid_type=orm.Str,
-            serializer=orm.to_aiida_type,
-            help="Relative to ini_basedir",
-        )
-        spec.input("ecraddir", valid_type=orm.RemoteData)
+        spec.input("ecrad_data", valid_type=orm.RemoteData)
+        spec.input("cloud_opt_props", valid_type=orm.RemoteData)
+        spec.input("dmin_wetgrowth_lookup", valid_type=orm.RemoteData)
+        spec.input("rrtmg_sw", valid_type=orm.RemoteData)
         spec.output("restart_file_dir")
         spec.output("restart_file_name")
+        spec.output("finish_status")
         options = spec.inputs["metadata"]["options"]  # type: ignore[index] # guaranteed correct by aiida-core
         options["resources"].default = {  # type: ignore[index] # guaranteed correct by aiida-core
             "num_machines": 10,
@@ -81,97 +40,59 @@ class Icon(engine.CalcJob):
             "num_cores_per_mpiproc": 2,
         }
         options["withmpi"].default = True  # type: ignore[index] # guaranteed correct by aiida-core
-        options["mpirun_extra_params"].default = [  # type: ignore[index] # guaranteed correct by aiida-core
-            "--ntasks-per-node",
-            "1",
-            "--hint=nomultithread",
-            "--cpus-per-task",
-            "1",
-        ]
-        options["parser_name"].default = "c2sm.spice_raw_icon"  # type: ignore[index] # guaranteed correct by aiida-core
-        options["environment_variables"].default = {  # type: ignore[index] # guaranteed correct by aiida-core
-            "GRAN_ICON": "core",
-            #      "OMP_NUM_THREADS": 1,
-            #      "ICON_THREADS": 1,
-            #      "OMP_SCHEDULE": "static,12",
-            #      "OMP_DYNAMIC": '"false"',
-            #      "OMP_STACKSIZE": "200M",
-            #      "NUM_THREAD_ICON": 1,
-            #      "NUM_IO_PROCS": 1,
-        }
+        options["parser_name"].default = "aiida_icon.icon"  # type: ignore[index] # guaranteed correct by aiida-core
         spec.exit_code(
             300,
             "ERROR_MISSING_OUTPUT_FILES",
             message="ICON did not create a restart file or directory!",
         )
+        spec.exit_code(
+            301,
+            "ERROR_READING_STATUS_FILE",
+            message="Could not read the finish.status file!",
+        )
+        spec.exit_code(
+            302,
+            "FAILED_CHECK_STATUS",
+            message="The final status was not 'OK', check the finish_status output!",
+        )
 
     def prepare_for_submission(self, folder: folders.Folder) -> datastructures.CalcInfo:
-        for outnum in range(1, 11):
-            folder.get_subfolder(f"out{outnum:02}", create=True)
+        model_namelist_data = f90nml.reads(self.inputs.model_namelist.get_content())
+
+        for output_spec in model_namelist_data["output_nml"]:
+            folder.get_subfolder(pathlib.Path(output_spec["output_filename"]).name, create=True)
 
         codeinfo = datastructures.CodeInfo()
         codeinfo.code_uuid = self.inputs.code.uuid
-
-        ini_basedir_path = pathlib.Path(self.inputs.ini_basedir.get_remote_path())
-        inidata_path = pathlib.Path(self.inputs.inidata.get_remote_path())
 
         calcinfo = datastructures.CalcInfo()
         calcinfo.codes_info = [codeinfo]
         calcinfo.remote_symlink_list = [
             (
                 self.inputs.code.computer.uuid,
-                self.inputs.gcm_converted.get_remote_path(),
-                "gcm_converted",
+                dynamics_grid_path := self.inputs.dynamics_grid_file.get_remote_path(),
+                pathlib.Path(dynamics_grid_path).name,
             ),
             (
                 self.inputs.code.computer.uuid,
-                self.inputs.boundary_data.get_remote_path(),
-                "boundary_data",
+                self.inputs.ecrad_data.get_remote_path(),
+                "ecrad_data",
             ),
             (
                 self.inputs.code.computer.uuid,
-                parent_grid_path := str(ini_basedir_path / self.inputs.parent_grid_relpath.value),
-                pathlib.Path(parent_grid_path).name,
-            ),
-            (
-                self.inputs.code.computer.uuid,
-                lam_grid_path := str(ini_basedir_path / self.inputs.lam_grid_relpath.value),
-                pathlib.Path(lam_grid_path).name,
-            ),
-            (
-                self.inputs.code.computer.uuid,
-                extpar_path := str(ini_basedir_path / self.inputs.extpar_relpath.value),
-                pathlib.Path(extpar_path).name,
-            ),
-            (
-                self.inputs.code.computer.uuid,
-                str(latbc_path := pathlib.Path(self.inputs.ini_basedir.get_remote_path()) / "dict.latbc"),
-                latbc_path.name,
-            ),
-            (
-                self.inputs.code.computer.uuid,
-                ecraddir_path := self.inputs.ecraddir.get_remote_path(),
-                pathlib.Path(ecraddir_path).name,
-            ),
-            (
-                self.inputs.code.computer.uuid,
-                str(inidata_path / self.inputs.ifs2icon_filename.value),
-                self.inputs.ifs2icon_filename.value,
+                self.inputs.rrtmg_sw.get_remote_path(),
+                "rrtmg_sw.nc",
             ),
         ]
-        if "restart_file_dir" in self.inputs:
-            restart_path = (
-                pathlib.Path(self.inputs.restart_file_dir.get_remote_path()) / self.inputs.restart_file_name.value
-            )
-            calcinfo.remote_symlink_list += [
-                (
-                    self.inputs.code.computer.uuid,
-                    str(restart_path),
-                    "multifile_restart_atm.mfr"
-                    if self.inputs.restart_file_name.value.startswith("multifile")
-                    else "restart_atm_DOM01.nc",
-                )
-            ]
+        calcinfo.remote_copy_list = [
+            (self.inputs.code.computer.uuid, self.inputs.cloud_opt_props.get_remote_path(), "ECHAM6_CldOptProps.nc"),
+            (
+                self.inputs.code.computer.uuid,
+                self.inputs.dmin_wetgrowth_lookup.get_remote_path(),
+                "dmin_wetgrowth_lookup.nc",
+            ),
+        ]
 
         calcinfo.local_copy_list = [
             (
@@ -182,24 +103,46 @@ class Icon(engine.CalcJob):
             (
                 self.inputs.model_namelist.uuid,
                 self.inputs.model_namelist.filename,
-                f"NAMELIST_{self.inputs.expid.value}",
+                "model.namelist",
             ),
         ]
+
+        if self.inputs.wrapper_script:
+            calcinfo.local_copy_list.append(
+                (
+                    self.inputs.wrapper_script.uuid,
+                    self.inputs.wrapper_script.filename,
+                    "run_icon.sh",
+                )
+            )
+        calcinfo.retrieve_list = ["finish.status", "nml.atmo.log", "output_schedule.txt"]
         return calcinfo
 
 
 class IconParser(parser.Parser):
     """Parser for raw Icon calculations."""
 
-    def parse(self, **kwargs):  # noqa: ARG002  # kwargs must be there for super compatibility
+    def parse(self, **kwargs):  # noqa: ARG002  # kwargs must be there for superclass compatibility
         remote_folder = self.node.outputs.remote_folder
 
         files = remote_folder.listdir()
         restart_pattern = re.compile(r".*_restart_atm_\d{8}T.*\.nc")
         multirestart_pattern = re.compile(r"multifile_restart_atm_\d{8}T.*.mfr")
+
         for file_name in files:
             if re.match(restart_pattern, file_name) or re.match(multirestart_pattern, file_name):
                 self.out("restart_file_name", orm.Str(file_name))
                 self.out("restart_file_dir", self.node.outputs.remote_folder.clone())
-                return engine.ExitCode(0)
-        return self.exit_codes.ERROR_MISSING_OUTPUT_FILES
+
+        if "finish.status" in self.retrieved.list_object_names():
+            try:
+                with self.retrieved.open("finish.status", "r") as status_file:
+                    out_status = status_file.read().strip()
+                    self.out("finish_status", orm.Str(out_status))
+                    if out_status == "OK":
+                        return engine.ExitCode(0)
+                    return self.exit_codes.FAILED_CHECK_STATUS
+            except OSError:
+                return self.exit_codes.ERROR_READING_STATUS_FILE
+        else:
+            return self.exit_codes.ERROR_MISSING_OUTPUT_FILES
