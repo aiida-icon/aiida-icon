e21312bf87229e280793833d2fc8940f869af536
DropD <rico.haeuselmann@gmail.com>
2024-09-09 12:02:12 +0200

    add convenience setup function for todi

--- examples/exclaim_R02B04/setup_and_run.py
+++ examples/exclaim_R02B04/setup_and_run.py
@@ -4,16 +4,17 @@ import pathlib
 import aiida
 import aiida.engine
 import aiida.orm
+from aiida_icon.site_support.cscs.todi import setup_for_todi_cpu
 
 COMPUTER_NAME = "todi"
 ICON_CODE_NAME = "icon"
+DRY_RUN = False
 
 if __name__ == "__main__":
     thisdir = pathlib.Path(__file__).parent.absolute()
-    icon = aiida.orm.load_code("icon-runscript@todi")
+    icon = aiida.orm.load_code(f"{ICON_CODE_NAME}@{COMPUTER_NAME}")
     make_remote_data = functools.partial(aiida.orm.RemoteData, computer=icon.computer)
     builder = icon.get_builder()
-    builder.wrapper_script = aiida.orm.SinglefileData(file=thisdir / "todi_cpu.sh")
     builder.master_namelist = aiida.orm.SinglefileData(file=thisdir / "icon_master.namelist")
     builder.model_namelist = aiida.orm.SinglefileData(file=thisdir / "NAMELIST_exclaim_ape_R02B04")
     builder.dynamics_grid_file = make_remote_data(
@@ -26,11 +27,12 @@ if __name__ == "__main__":
     builder.metadata.options.mpirun_extra_params = [
         "--threads-per-core=1",
         "--distribution=block:block:block",
-        "--uenv=icon-wcp/v1:rc4",
-        "./run_icon.sh",
     ]
     builder.metadata.description = "Icon on Todi through via wrapper script."
     builder.metadata.options.resources = {"num_machines": 1, "num_mpiprocs_per_machine": 288}
     builder.metadata.options.max_memory_kb = 128000000
-    builder.metadata.dry_run = False
+    builder.metadata.dry_run = DRY_RUN
+
+    setup_for_todi_cpu(builder)
+
     print(aiida.engine.submit(builder))
---
+++ src/aiida_icon/site_support/cscs/todi.py
@@ -0,0 +1,48 @@
+import pathlib
+import textwrap
+
+import aiida
+from aiida.engine import processes
+
+SCRIPT_DIR = pathlib.Path(__file__).parent.absolute() / "wrapper_scripts"
+
+
+# TODO(ricoh): allow custom images
+def common_alps_setup(builder: processes.ProcessBuilder) -> None:
+    """
+    Set AiiDA process options for running an aiida_icon.icon calcjob on ALPS.
+
+    Needs to be used with the `wrapper_script` input of the IconCalcjob
+    """
+    options = builder.metadata.options  # type: ignore[attr-defined]  # builder has a custom setattr
+    options.custom_scheduler_commands = "\n".join(
+        [
+            *options.custom_scheduler_commands.splitlines(),
+            "#SBATCH --uenv=icon-wcp/v1:rc4",
+        ]
+    )
+    options.environment_variables = {
+        "CUDA_BUFFER_PAGE_IN_THRESHOLD": "0.001",
+        "FI_CXI_SAFE_DEVMEM_COPY_THRESHOLD": "0",
+        "FI_CXI_RX_MATCH_NODE": "software",
+        "FI_MR_CACHE_MONITOR": "disabled",
+        "MPICH_GPU_SUPPORT_ENABLED": "1",
+        "NVCOMPILER_ACC_DEFER_UPLOADS": "1",
+        "NVCOMPILER_TERM": "trace",
+        "OMP_NUM_THREADS": "1",
+        "ICON_THREADS": "1",
+        "OMP_SCHEDULE": "static,1",
+        "OMP_DYNAMIC": "false",
+        "OMP_STACKSIZE": "200M",
+    }
+    options.prepend_text = textwrap.dedent(
+        """
+        chmod 755 run_icon.sh
+        """
+    )
+    options.mpirun_extra_params.append("./run_icon.sh")
+
+
+def setup_for_todi_cpu(builder: processes.ProcessBuilder) -> None:
+    common_alps_setup(builder)
+    builder.wrapper_script = aiida.orm.SinglefileData(file=SCRIPT_DIR / "todi_cpu.sh")
